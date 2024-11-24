1735837ce6c07ba558860a50e0cbfd44ed29e881
bernoussama <oussama.bernou@outlook.com>
2024-11-24 02:29:13 +0100

    update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
-mkdir -p ~/mercury
+mkdir -p ~/mercury/zones
 cd ~/mercury
+wget https://raw.githubusercontent.com/bernoussama/mercury/main/compose.yaml -O compose.yaml
 read -p "run as root? (y/n) " yn
 if [ "$yn" = "y" ]; then
   echo "running as root"
