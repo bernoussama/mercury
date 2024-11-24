36d8b7efa85b366ca01f3023f4447fc5184455fb
bernoussama <oussama.bernou@outlook.com>
2024-11-24 02:42:38 +0100

    update compose and install.sh

--- install.sh
+++ install.sh
@@ -5,13 +5,12 @@ mkdir -p ~/mercury/zones
 cd ~/mercury
 
 # wget https://raw.githubusercontent.com/bernoussama/mercury/main/zones/example.yml -O zones/example.yml;
-wget https://raw.githubusercontent.com/bernoussama/mercury/main/Dockerfile -O Dockerfile
 wget https://raw.githubusercontent.com/bernoussama/mercury/main/compose.yaml -O compose.yaml
 
-read -p "port: " -s PORT
+read -p "port: " PORT
 export PORT
 
-read -p "run as root? (y/n) " -s yn
+read -p "run as root? (y/n) " yn
 
 if [ "$yn" = "y" ]; then
   echo "running as root"
